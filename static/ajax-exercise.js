20d5c3c4f2b9ea7bf18acb7cc41a94f8df1aded7
Your Name <you@example.com>
2020-02-28 11:55:40 -0800

    finished part 1 and 2

---
+++ static/ajax-exercise.js
@@ -0,0 +1,55 @@
+"use strict";
+
+
+// PART 1: SHOW A FORTUNE
+
+function showFortune(evt) {
+
+    // TODO: get the fortune and show it in the #fortune-text div
+    // evt.preventDefault();
+    $.get('/fortune', (response) => {
+        $('#fortune-text').text(response);
+});
+
+}
+
+$('#get-fortune-button').on('click', showFortune);
+
+
+
+
+
+
+
+// PART 2: SHOW WEATHER
+
+function showWeather(evt) {
+    evt.preventDefault();
+
+    let url = "/weather.json";
+    let formData = {"zipcode": $("#zipcode-field").val()};
+
+    $.get(url, formData, (res) => {
+      $('#weather-info').text(res.forecast);
+    });
+    console.log(url);
+    // TODO: request weather with that URL and show the forecast in #weather-info
+}
+
+$("#weather-form").on('submit', showWeather);
+
+
+
+
+// PART 3: ORDER MELONS
+
+function orderMelons(evt) {
+    evt.preventDefault();
+
+    // TODO: show the result message after your form
+    // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+}
+
+$("#order-form").on('submit', orderMelons);
+
+
